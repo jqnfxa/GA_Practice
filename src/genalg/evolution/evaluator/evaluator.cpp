77d3e2bc82167793a5108e71d8fe080d916191fb
jqnfxa <ivborisov@stud.etu.ru>
2024-07-01 20:19:30 +0300

    fix: general algorithms connection

--- src/genalg/evolution/evaluator/evaluator.cpp
+++ src/genalg/evolution/evaluator/evaluator.cpp
@@ -5,7 +5,7 @@ PolynomialEvaluator::PolynomialEvaluator(const Polynomial<genom_t> &polynomial)
 	: polynomial_(polynomial) {}
 
 
-double PolynomialEvaluator::evaluate(const genom_t &genom)
+double PolynomialEvaluator::evaluate(const genom_t &genom) const
 {
 	return polynomial_(genom);
 }
